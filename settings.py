e2860dc39cf512ece81c995bd35161d6c52a4596
Ro-ee Tal <roeetal1@gmail.com>
2021-06-14 14:21:26 -0700

    housing

--- settings.py
+++ settings.py
@@ -13,8 +13,9 @@ CATEGORY = 'hhh'
 FILTERS = extra_filters = {
     'has_image': True,
     'max_price': 1400,
-    'search_distance': 5,
+    'search_distance': 3,
     'bundle_duplicates': True,
     'zip_code': 'V5T2Z3',
+    # 'zip_code': 'V7M1B1',
     'laundry': 'w/d in unit',
 }
